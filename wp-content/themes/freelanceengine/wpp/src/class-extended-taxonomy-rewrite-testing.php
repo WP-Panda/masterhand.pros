8d4790686111612fc9728bcdcbf88c09409cea93
wppanda <yoowordpress@yandex.ru>
2021-09-17 20:26:29 +0300

    CPT NEW OPTIONS

---
+++ wp-content/themes/freelanceengine/wpp/src/class-extended-taxonomy-rewrite-testing.php
@@ -0,0 +1,36 @@
+<?php
+
+defined('ABSPATH') || exit;
+
+class Extended_Taxonomy_Rewrite_Testing extends Extended_Rewrite_Testing {
+
+	/**
+	 * @var Extended_Taxonomy
+	 */
+	public $taxo;
+
+	public function __construct( Extended_Taxonomy $taxo ) {
+		$this->taxo = $taxo;
+	}
+
+	public function get_tests(): array {
+		global $wp_rewrite;
+
+		if ( ! $wp_rewrite->using_permalinks() ) {
+			return [];
+		}
+
+		if ( ! isset( $wp_rewrite->extra_permastructs[ $this->taxo->taxonomy ] ) ) {
+			return [];
+		}
+
+		$struct     = $wp_rewrite->extra_permastructs[ $this->taxo->taxonomy ];
+		$tax        = get_taxonomy( $this->taxo->taxonomy );
+		$name       = sprintf( '%s (%s)', $tax->labels->name, $this->taxo->taxonomy );
+
+		return [
+			$name => $this->get_rewrites( $struct, [] ),
+		];
+	}
+
+}
