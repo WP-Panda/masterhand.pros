8a3013b25fc9d03fdc935618bac8c7fb14a670ad
wppanda <yoowordpress@yandex.ru>
2021-10-03 18:55:13 +0300

    skills config

---
+++ wp-content/themes/freelanceengine/wpp/modules/wpp-rating/config.php
@@ -0,0 +1,179 @@
+<?php
+function wpp_rating_config() {
+	$array = [];
+
+	$array['messages'] = [
+		'settings'   => __( 'Settings', WPP_TEXT_DOMAIN ),
+		'back'       => __( 'Back', WPP_TEXT_DOMAIN ),
+		'refresh'    => __( 'Refresh', WPP_TEXT_DOMAIN ),
+		'title'      => __( 'Headline', WPP_TEXT_DOMAIN ),
+		'newSkill'   => __( 'New skill', WPP_TEXT_DOMAIN ),
+		'countUsed'  => __( 'Number of uses', WPP_TEXT_DOMAIN ),
+		'readMore'   => __( 'more details', WPP_TEXT_DOMAIN ),
+		'status'     => __( 'Status', WPP_TEXT_DOMAIN ),
+		'apply'      => __( 'To apply', WPP_TEXT_DOMAIN ),
+		'save'       => __( 'Save', WPP_TEXT_DOMAIN ),
+		'edit'       => __( 'Edit', WPP_TEXT_DOMAIN ),
+		'submit'     => __( 'Submit', WPP_TEXT_DOMAIN ),
+		'cancel'     => __( 'Cancel', WPP_TEXT_DOMAIN ),
+		'search'     => __( 'Search', WPP_TEXT_DOMAIN ),
+		'reset'      => __( 'Reset', WPP_TEXT_DOMAIN ),
+		'send'       => __( 'Submit', WPP_TEXT_DOMAIN ),
+		'create'     => __( 'Create', WPP_TEXT_DOMAIN ),
+		'toSet'      => __( 'Set', WPP_TEXT_DOMAIN ),
+		'action'     => __( 'Act', WPP_TEXT_DOMAIN ),
+		'editSkill'  => __( 'Edit skill', WPP_TEXT_DOMAIN ),
+		'group'      => __( 'Group', WPP_TEXT_DOMAIN ),
+		'name'       => __( 'Name', WPP_TEXT_DOMAIN ),
+		'view'       => __( 'View', WPP_TEXT_DOMAIN ),
+		'detail'     => __( 'More details', WPP_TEXT_DOMAIN ),
+		'created'    => __( 'Created by', WPP_TEXT_DOMAIN ),
+		'open'       => __( 'Open', WPP_TEXT_DOMAIN ),
+		'delete'     => __( 'Delete', WPP_TEXT_DOMAIN ),
+		'deleted'    => __( 'Deleted', WPP_TEXT_DOMAIN ),
+		'error'      => __( 'Error!', WPP_TEXT_DOMAIN ),
+		'freelancer' => __( 'Freelancer', WPP_TEXT_DOMAIN ),
+		'employer'   => __( 'Employer', WPP_TEXT_DOMAIN )
+	];
+
+	$array['fields'] = [
+		'coefficient_pro_status'          => [
+			'label' => __( 'Coef. rating growth from PRO status, %', WPP_TEXT_DOMAIN ),
+			'for'   => 'all',
+			'def'   => 50
+		],
+		'coefficient_amount_payment'      => [
+			'label' => __( 'Purchases & money transactions (for 1$)', WPP_TEXT_DOMAIN ),
+			'for'   => 'all',
+			'def'   => 1
+		],
+		'site_visit'                      => [
+			'label' => __( 'Site visits', WPP_TEXT_DOMAIN ),
+			'for'   => 'all',
+			'def'   => 5
+		],
+		'one_field_profile'               => [
+			'label' => __( 'Completed profile info', WPP_TEXT_DOMAIN ),
+			'for'   => 'all',
+			'def'   => 10
+		],
+		'freelancer_one_portfolio'        => [
+			'label' => __( 'For Number of Portfolio Jobs', WPP_TEXT_DOMAIN ),
+			'for'   => '',
+			'def'   => 10
+		],
+		'freelancer_as_referral'          => [
+			'label' => __( 'As referral', WPP_TEXT_DOMAIN ),
+			'for'   => 'freelancer',
+			'def'   => 500
+		],
+		'freelancer_as_referrer'          => [
+			'label' => __( 'As referrer', WPP_TEXT_DOMAIN ),
+			'for'   => 'freelancer',
+			'def'   => 2000
+		],
+		'freelancer_for_reward'           => [
+			'label' => __( 'Rewards', WPP_TEXT_DOMAIN ),
+			'for'   => 'freelancer',
+			'def'   => 1000
+		],
+		'freelancer_for_review'           => [
+			'label' => __( 'Reviews', WPP_TEXT_DOMAIN ),
+			'for'   => 'freelancer',
+			'def'   => 50
+		],
+		'freelancer_for_skill'            => [
+			'label' => __( 'Skills', WPP_TEXT_DOMAIN ),
+			'for'   => 'freelancer',
+			'def'   => 10
+		],
+		'freelancer_for_endorse_skill'    => [
+			'label' => __( 'Approved skills', WPP_TEXT_DOMAIN ),
+			'for'   => 'freelancer',
+			'def'   => 10
+		],
+		'freelancer_from_rating_employer' => [
+			'label' => __( 'Coef. from client rating for a successfully closed project, %', WPP_TEXT_DOMAIN ),
+			'for'   => 'freelancer',
+			'def'   => 50
+		],
+		'employer_from_rating_freelancer' => [
+			'label' => __( 'Coef. from professional rating for a successfully closed project, %', WPP_TEXT_DOMAIN ),
+			'for'   => 'employer',
+			'def'   => 50
+		],
+		'freelancer_project_success'      => [
+			'label' => __( 'Successfully closed project (via SafePay Deal)', WPP_TEXT_DOMAIN ),
+			'for'   => 'freelancer',
+			'def'   => 50
+		],
+		/*'freelancer_installment_plan'     => [
+			'label' => __( 'Trusted Partner program participation', WPP_TEXT_DOMAIN ),
+			'for'   => 'freelancer',
+			'def'   => 50
+		],
+		'employer_installment_plan'       => [
+			'label' => __( 'Trusted Partner program participation', WPP_TEXT_DOMAIN ),
+			'for'   => 'employer',
+			'def'   => 50
+		],*/
+		'employer_as_referral'            => [
+			'label' => __( 'As referral', WPP_TEXT_DOMAIN ),
+			'for'   => 'employer',
+			'def'   => 50
+		],
+		'employer_as_referrer'            => [
+			'label' => __( 'As referrer', WPP_TEXT_DOMAIN ),
+			'for'   => 'employer',
+			'def'   => 50
+		],
+		'employer_for_reward'             => [
+			'label' => __( 'Rewards', WPP_TEXT_DOMAIN ),
+			'for'   => 'employer',
+			'def'   => 50
+		],
+		'employer_for_review'             => [
+			'label' => __( 'Reviews', WPP_TEXT_DOMAIN ),
+			'for'   => 'employer',
+			'def'   => 50
+		],
+		'employer_project_success'        => [
+			'label' => __( 'Successfully closed project (via SafePay Deal)', WPP_TEXT_DOMAIN ),
+			'for'   => 'employer',
+			'def'   => 50
+		],
+		'employer_bid_accepted'           => [
+			'label' => __( 'For the selected project PRO executor', WPP_TEXT_DOMAIN ),
+			'for'   => 'employer',
+			'def'   => 50
+		],
+		'employer_for_skill'              => [
+			'label' => __( 'Skills', WPP_TEXT_DOMAIN ),
+			'for'   => 'employer',
+			'def'   => 50
+		],
+		'employer_for_endorse_skill'      => [
+			'label' => __( 'Approved skills', WPP_TEXT_DOMAIN ),
+			'for'   => 'employer',
+			'def'   => 50
+		]
+	];
+
+	$array['errors'] = [
+		'name_skill_empty'  => __( 'Empty value', WPP_TEXT_DOMAIN ),
+		'name_skill_exists' => __( 'Such a value exists', WPP_TEXT_DOMAIN ),
+		'not_found_record'  => __( 'Database entry not found', WPP_TEXT_DOMAIN )
+	];
+
+	$array['js'] = [
+		'yes'           => __( 'Yes', WPP_TEXT_DOMAIN ),
+		'no'            => __( 'No', WPP_TEXT_DOMAIN ),
+		'success'       => __( 'Success', WPP_TEXT_DOMAIN ),
+		'error'         => __( 'Error!', WPP_TEXT_DOMAIN ),
+		'delete_record' => __( 'Delete record', WPP_TEXT_DOMAIN ),
+		'sure'          => __( 'Sure?', WPP_TEXT_DOMAIN )
+	];
+
+	return apply_filters( 'wpp_rating_setting', $array );
+
+}
